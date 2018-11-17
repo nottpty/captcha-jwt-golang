0880b8221fa8947479efad1a3f9dcbc311c41097
Wadjanan Klinnooch <se57160387@gmail.com>
2018-11-17 10:56:33 +0700

    Change One to Two

--- captcha.go
+++ captcha.go
@@ -17,13 +17,6 @@ func Captcha(pattern, leftoperand, operator, rightoperand int) captcha {
 	return captcha{}
 }
 
-<<<<<<< HEAD
-// Number change integer to string
-func Number() string {
-
-	return ""
-=======
 func Number(n int) string {
-	return "one"
->>>>>>> 690bdf2d0bee307b30575e146e683cc854d8b03c
+	return "two"
 }
