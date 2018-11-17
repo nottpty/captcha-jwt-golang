3c6b00e3b4a7e7c0c75ab316331b4c455dac238c
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2018-11-17 10:28:41 +0700

    test number one as string, just test case

--- captcha_test.go
+++ captcha_test.go
@@ -2,6 +2,12 @@ package captcha
 
 import "testing"
 
+func TestNumberOneAsString(t *testing.T) {
+	if Number(1) != "one" {
+		t.Error("it should be one but get", Number(1))
+	}
+}
+
 func TestCaptchaFirstPatternOfOnePlusOne(t *testing.T) {
 	c := Captcha(FirstPattern, 1, OperationPlus, 1)
 	if c.String() != "1 + one" {
